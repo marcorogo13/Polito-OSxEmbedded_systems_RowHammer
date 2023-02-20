02c4303c861c5303c4c1ede76fad745f0614b1f6
Marco Rosa Gobbo <81075490+marcorogo13@users.noreply.github.com>
2023-02-20 14:59:52 +0100

    Update main.c

--- main.c
+++ main.c
@@ -69,81 +69,81 @@ int main(){
 
 
     /*start of actual hammer program*/
+    while (1){
+        do{
+            printf("Select what you want to try: \n1)ZVA \n2)DC CVAC \n3)DC CIVAC \n");
+            scanf("%d", &selection);
+            if(!(selection == 1 || selection == 2 || selection == 3)){
+                printf("Wrong input (%d), Please select a correct value\n", selection);
+            }
+        } while(!(selection == 1 || selection == 2 || selection == 3));
 
-    do{
-        printf("Select what you want to try: \n1)ZVA \n2)DC CVAC \n3)DC CIVAC \n");
-        scanf("%d", &selection);
-        if(!(selection == 1 || selection == 2 || selection == 3)){
-            printf("Wrong input (%d), Please select a correct value\n", selection);
+        printf("Enter number of refresh cycles you want to run the test for:\n");
+        scanf("%d", &number_of_cycles);
+
+
+        //The number of effective cycle for the program to run, e.g. how many refresh cycles should we test
+        cycles = HAMMER_ROUND * number_of_cycles;
+
+        switch (selection)
+        {
+        case 1:
+            for (j = 0; j < cycles; ++j) {
+                asm volatile(
+                    "str %2, [%0]\n\t"
+                    "str %2, [%1]\n\t"
+                    "dc zva, %0\n\t"
+                    "dc zva, %1\n\t"
+                    ::"r" (addr1), "r" (addr2), "r" (temporary)
+              );
+            }
+            break;
+
+
+        case 2:
+            for (j = 0; j < cycles; ++j) {
+                asm volatile(
+                    "str %2, [%0]\n\t"
+                    "str %2, [%1]\n\t"
+                    "dc cvac, %0\n\t"
+                    "dc cvac, %1\n\t"
+                    ::"r" (addr1), "r" (addr2), "r" (temporary)
+              );
+            }
+            break;
+
+        case 3:
+            for (j = 0; j < cycles; ++j) {
+                asm volatile(
+                    "str %2, [%0]\n\t"
+                    "str %2, [%1]\n\t"
+                    "dc civac, %0\n\t"
+                    "dc civac, %1\n\t"
+                    ::"r" (addr1), "r" (addr2), "r" (temporary)
+              );
+            }
+             break;
+        default:
+                printf("Invalid choice.\n");
+                return 1;
+            break;
         }
-    } while(!(selection == 1 || selection == 2 || selection == 3));
 
-    printf("Enter number of refresh cycles you want to run the test for:\n");
-    scanf("%d", &number_of_cycles);
+        /*test to see if there are any flipped bits*/
 
+        for (int i = 0; i < mapping_size; i++){
+            if(( *((int8_t*)mapped + i) == 1){
+                printf("Found a bit flip in position : %d\n", i);
+                found_flag ++;    
+            }
+        }    
 
-    //The number of effective cycle for the program to run, e.g. how many refresh cycles should we test
-    cycles = HAMMER_ROUND * number_of_cycles;
-
-    switch (selection)
-    {
-    case 1:
-        for (j = 0; j < cycles; ++j) {
-            asm volatile(
-                "str %2, [%0]\n\t"
-                "str %2, [%1]\n\t"
-                "dc zva, %0\n\t"
-                "dc zva, %1\n\t"
-                ::"r" (addr1), "r" (addr2), "r" (temporary)
-          );
+        if (!found_flag){
+            printf("No bit-flip found :( \n");
+        } else{
+            printf("%d bit flips found :).\n", found_flag);
         }
-        break;
-
-
-    case 2:
-        for (j = 0; j < cycles; ++j) {
-            asm volatile(
-                "str %2, [%0]\n\t"
-                "str %2, [%1]\n\t"
-                "dc cvac, %0\n\t"
-                "dc cvac, %1\n\t"
-                ::"r" (addr1), "r" (addr2), "r" (temporary)
-          );
-        }
-        break;
-
-    case 3:
-        for (j = 0; j < cycles; ++j) {
-            asm volatile(
-                "str %2, [%0]\n\t"
-                "str %2, [%1]\n\t"
-                "dc civac, %0\n\t"
-                "dc civac, %1\n\t"
-                ::"r" (addr1), "r" (addr2), "r" (temporary)
-          );
-        }
-         break;
-    default:
-            printf("Invalid choice.\n");
-            return 1;
-        break;
     }
-    
-    /*test to see if there are any flipped bits*/
-
-    for (int i = 0; i < mapping_size; i++){
-        if(( *((int8_t*)mapped + i) == 1){
-            printf("Found a bit flip in position : %d\n", i);
-            found_flag ++;    
-        }
-    }    
-
-    if (!found_flag){
-        printf("No bit-flip found :( \n");
-    } else{
-        printf("%d bit flips found :).\n", found_flag);
-    }
-
 
     return 0;
 }
